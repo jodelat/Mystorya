cb6d4d1a93b791a89cfe440da27e1a857dea44d3
MMMamich <MMMamich@gmail.com>
2017-10-31 13:39:05 -0700

    trying something out

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3030;
+var PORT = process.env.PORT;
 
 var db = require("./models");
 
@@ -22,6 +22,3 @@ db.sequelize.sync({}).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-
-
